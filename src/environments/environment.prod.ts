afbf2e6596d4e523591a98787a071f4720733169
Fatih-esgi <fatih@fat-e.ch>
2021-04-21 14:16:24 +0200

    install & setup firestore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAzMPw--PkLBZT-_JbQty-Iof7kv152RYI",
+    authDomain: "additivelist.firebaseapp.com",
+    projectId: "additivelist",
+    storageBucket: "additivelist.appspot.com",
+    messagingSenderId: "1091840266794",
+    appId: "1:1091840266794:web:517a31878c753a661d6953"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAzMPw--PkLBZT-_JbQty-Iof7kv152RYI",
+    authDomain: "additivelist.firebaseapp.com",
+    projectId: "additivelist",
+    storageBucket: "additivelist.appspot.com",
+    messagingSenderId: "1091840266794",
+    appId: "1:1091840266794:web:517a31878c753a661d6953"
+  }
 };
 
 /*
