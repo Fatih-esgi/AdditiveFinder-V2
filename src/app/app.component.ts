f4888c52d95a8ac0d142272a51d958b3588b1b64
Fatih-esgi <fatih@fat-e.ch>
2021-04-21 15:23:30 +0200

    :upd link comp to nav & change icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FavorisComponent } from './@features/favoris/favoris.component';
+import { FavorisModule } from './@features/favoris/favoris.module';
+import { ListeAdditiveComponent } from './@features/liste-additive/liste-additive.component';
+import { ListeAdditiveModule } from './@features/liste-additive/liste-additive.module';
+import { SearchAdditiveComponent } from './@features/search-additive/search-additive.component';
+import { SearchAdditiveModule } from './@features/search-additive/search-additive.module';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +14,9 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'additiveFinderV2';
+  linkToSearch = SearchAdditiveComponent;
+  linkToListe = ListeAdditiveComponent;
+  linkToFav = FavorisComponent;
   
   constructor(
 
